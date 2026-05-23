0ff98d824cac7552d8d7db9b6d822cb404e6019d
pawankumar-arya <pawankumararya.in@gmail.com>
2026-05-23 12:07:05 +0530

    loop complete

---
+++ Loops/while-loop/EvenNum.java
@@ -0,0 +1,11 @@
+public class EvenNum {
+  public static void main(String[]args){
+    int num = 1;
+    while(num <= 10){
+      if(num%2==0){
+        System.out.println(num);
+      }
+      num++ ;
+    }
+  }
+}
